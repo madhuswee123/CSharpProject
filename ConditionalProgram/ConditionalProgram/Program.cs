e832c1605ec721b20a544de5096531724d669dcd
madhura venkatesh <madhura_venkatesh@epam.com>
2025-03-11 12:46:13 +0530

    Added Conditional Changes and Factorial Projects

---
+++ ConditionalProgram/ConditionalProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> teamMembers = new List<string>();
+
+            // Taking input from user
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Enter the name of team member {i + 1}: ");
+                teamMembers.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nTeam Members:");
+            foreach (string member in teamMembers)
+            {
+                Console.WriteLine(member);
+               // Console.ReadLine();
+            }
+            // Finding the longest name
+            string longestName = teamMembers.OrderByDescending(name => name.Length).FirstOrDefault();
+            Console.WriteLine($"\nThe longest name in the team is: {longestName}");
+        }
+    }
+}
---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to calculate its factorial: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+            }
+            else
+            {
+                long factorial = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine($"The factorial of {number} is: {factorial}");
+            }
+        }
+    }
+}
